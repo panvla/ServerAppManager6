639949a27b4468a83a389e4abf9d1e5866f3c6fc
panvla <pandvlado@gmail.com>
2023-04-06 20:31:57 +0200

    added initial project.

---
+++ serverAppManagerB/src/main/java/com/vladimirpandruov/serverAppManagerB/ServerAppManagerBApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandruov.serverAppManagerB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppManagerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppManagerBApplication.class, args);
+	}
+
+}
---
+++ serverAppManagerB/src/test/java/com/vladimirpandruov/serverAppManagerB/ServerAppManagerBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandruov.serverAppManagerB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppManagerBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
